8659fc2115880a588a07bbc25b5fef477cde77c3
Ayush Khanal <vegetablebat1@gmail.com>
2018-03-17 23:43:45 -0600

    homework 7 changes

---
+++ Assignments/Hmwk7/libraryDriver.cpp
@@ -0,0 +1,89 @@
+// Author: Ayush Khanal CS1300 Fall 2017
+// Recitation: 204- Chelsea Chandler
+// Cloud9 Workspace Editor Link: https://ide.c9.io/ayushkhanal/ayushkhanalcsci1300
+// Homework 7 part 1, all problems
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+        we probably need some fileio algorithims and for loops and object array algorithims
+        writing psuedocode for a class is weird without knowing how all this will actually work
+    */
+	// Put your algorithms here. We have provided an example below of loading from the external data files.
+	//
+	//
+	// create new Library object which will have all users in an array and all books in an array 
+	// Read the file books.txt 
+	// Read the file users.txt 
+    /*	
+            class Library
+        {
+            public members:
+                setUsers function
+                    -input filestream that reads data and constructs users objects and constructs user objects and puts into an array
+                setBooks function
+                    -input filestream that reads data and constrcuts books objects and puts them into an array
+
+                addUser function
+                    - utilizes the user class and its methods
+                login function
+                quit function
+                    -output filestream
+                view function
+                rate function
+                recomender function
+            private members:
+                array of books objects
+                array of users objects
+                    -will need necessary syntax for object arrays
+
+        }
+        Library:: setUsers
+        {
+            use the User class to create arrays of users
+        }
+
+        Library::setBooks
+        {
+            use the books class to create arrays of books
+        }
+        Library::view
+        {
+            will use the two classes to print out ratings
+        }
+        Library::quit
+        {
+            we will write this function just some fileio
+        }
+        Library::addUser
+        {
+        use the user class and its methods
+        }
+        Library::rate
+        {
+        use the book class and its functions
+        }
+        Library::reccomend
+        {
+        we will use the algorithim provided
+        }
+        */
+	// Check if file open fails for either file 
+	//		if a file open fails, print "Error. User file or book file could not be found. Please make sure that users.txt and books.txt is in the same directory."
+	//		exit program 
+	// Create a book object for each book in the book file 
+	// Create a user object for each user in the book file 
+	// present menu to the user
+	// the menu should have the following options: 
+	//		quit
+	//		...
+	//		...
+	//	Continue elaborating what should happen if the user selects other menu options. You should also think about what happens 
+	//	when the user provides invalid input, or some operation cannot occur. 
+    return 0;
+}
